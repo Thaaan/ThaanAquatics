4a2ddd07f7bec78ca296f3d4318bcf9b4683ec0a
Thaaan <ethalo10@gmail.com>
2023-10-31 18:14:25 -0700

    more fixes

--- assets/js/server.js
+++ assets/js/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
