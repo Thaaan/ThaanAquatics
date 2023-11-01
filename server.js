65d4beacd2f6657317067022b4e36157ff1e43b4
Thaaan <ethalo10@gmail.com>
2023-10-31 18:32:01 -0700

    should be fixed

--- server.js
+++ server.js
@@ -2,13 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
-
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
+  const open = await import('open');
+  open.default(`http://localhost:${port}`);
 });
