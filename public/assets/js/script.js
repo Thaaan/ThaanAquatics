ae4300b282694189bd3002079670eec73c3be9e7
Thaaan <ethalo10@gmail.com>
2023-11-03 00:50:02 -0700

    cart functionality

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -27,4 +27,132 @@ document.addEventListener("DOMContentLoaded", function() {
             searchButton.classList.remove('no-transition');
         }, 100);
     });
+
+    const addToCartButtons = document.querySelectorAll('.add-to-cart-btn');
+
+    addToCartButtons.forEach((button) => {
+        button.addEventListener('click', function() {
+            const productName = this.getAttribute('data-product-name');
+            const productPrice = this.getAttribute('data-product-price');
+            addToCart(productName, productPrice, 1);
+        });
+    });
+
+    const cartIcon = document.getElementById('cart-icon');
+    const cartDropdown = document.querySelector('.cart-dropdown');
+
+    cartIcon.addEventListener('click', function() {
+        updateCartDropdown();
+        // Toggle dropdown visibility
+        cartDropdown.style.display = cartDropdown.style.display === 'block' ? 'none' : 'block';
+    });
+
+    document.addEventListener('mousedown', function(event) {
+        if (!cartDropdown.contains(event.target) && !cartIcon.contains(event.target)) {
+            cartDropdown.style.display = 'none';
+        }
+    });
 });
+
+function setCartCookie(cart) {
+    // Serialize the cart object to a JSON string
+    const cartValue = JSON.stringify(cart);
+    // Set the cookie to expire in 1 day/s
+    const date = new Date();
+    date.setTime(date.getTime() + (24 * 60 * 60 * 1000));
+    const expires = "expires=" + date.toUTCString();
+    // Set the cookie with the cart data
+    document.cookie = "cart=" + cartValue + ";" + expires + ";path=/";
+    console.log('Cookie set: ', document.cookie);
+}
+
+function addToCart(productName, productPrice, quantity) {
+    let cart = getCartFromCookie();
+    let cartItem = cart.find(item => item.productName === productName);
+
+    if (cartItem) {
+        cartItem.quantity += quantity;
+    } else {
+        cart.push({
+            productName: productName,
+            productPrice: productPrice,
+            quantity: quantity
+        });
+    }
+
+    setCartCookie(cart);
+}
+
+function getCartFromCookie() {
+    // Retrieve the cookie value
+    const name = "cart=";
+    const decodedCookie = decodeURIComponent(document.cookie);
+    const ca = decodedCookie.split(';');
+
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i].trim(); // Use trim() to remove whitespace from both ends of the string
+        if (c.indexOf(name) === 0) {
+            try {
+                // Parse the JSON string back to an object
+                return JSON.parse(c.substring(name.length));
+            } catch (e) {
+                console.error('Error parsing cart cookie:', e);
+                return [];
+            }
+        }
+    }
+    // Return an empty array if the cart cookie does not exist
+    return [];
+}
+
+function removeItemFromCart(index) {
+    let cart = getCartFromCookie();
+    // Remove the item at the specified index
+    cart.splice(index, 1);
+    // Update the cart cookie
+    setCartCookie(cart);
+    // Update the cart dropdown to reflect the changes
+    updateCartDropdown();
+}
+
+function updateCartDropdown() {
+    const cart = getCartFromCookie();
+    const cartItemsContainer = document.querySelector('.cart-items');
+    cartItemsContainer.innerHTML = ''; // Clear current cart items
+
+    let subtotal = 0;
+    cart.forEach((item, index) => {
+        const listItem = document.createElement('li');
+        listItem.className = 'cart-item-grid'; // Add a class for styling
+        listItem.innerHTML = `
+            <span class="cart-item-name">${item.productName}</span>
+            <span class="cart-item-quantity">Qty: ${item.quantity}</span>
+            <button class="delete-item" data-index="${index}">Remove</button>
+        `;
+        cartItemsContainer.appendChild(listItem);
+
+        // Calculate subtotal
+        subtotal += item.productPrice * item.quantity;
+    });
+
+    // Add subtotal and checkout button
+    const checkoutSection = document.createElement('div');
+    checkoutSection.className = 'checkout-section';
+    checkoutSection.innerHTML = `
+        <div class="subtotal">Subtotal: $${subtotal.toFixed(2)}</div>
+        <button class="checkout-button">Checkout</button>
+    `;
+    cartItemsContainer.appendChild(checkoutSection);
+
+    if (cart.length === 0) {
+        cartItemsContainer.innerHTML = '<li>Your cart is empty.</li>';
+    } else {
+        // Add event listeners to the delete buttons after the list is populated
+        document.querySelectorAll('.delete-item').forEach(button => {
+            button.addEventListener('click', function(event) {
+                event.stopPropagation(); // Prevent the dropdown from closing
+                removeItemFromCart(this.getAttribute('data-index'));
+            });
+        });
+    }
+}
